961a9dbebc46f47e753e79e73a2f29e52adf41dd
Thomas <thomaslee9474@gmail.com>
2019-06-11 13:49:31 -0700

    [added] errors

---
+++ error_handling.py
@@ -0,0 +1,11 @@
+try:
+  pass
+except Exception:
+  pass
+else:
+  pass
+finally:
+  pass
+
+# Exception is a general catch all catch, it wont tell you anything specific if you dont know 
+# what the error is. It is better to create your own error handling module
